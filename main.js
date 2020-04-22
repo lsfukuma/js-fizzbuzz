5ad7b1200f0164751d679d54802f8648689a156a
Leticia <lsf@protonmail.ch>
2020-04-22 15:51:07 +0200

    x15 ok

--- main.js
+++ main.js
@@ -4,19 +4,19 @@
 //    al posto dei multipli di 5 stampi "Buzz"
 //    al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
 
-//Scrivere un array contenenti i numeri da1 a 100
+//Scrivere un array contenenti i numeri da 1 a 100
 var array_numbers = [];
 
 for (var i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-    //al posto dei multipli di 3 stampi "Fizz"
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
+    if (i % 3 == 0 && i % 5 == 0) {
+    //al posto dei multipli di 15 stampi "FizzBuzz"
+        console.log('FizzBuzz');
+    } else if (i % 5 == 0) {
     //al posto dei multipli di 5 stampi "Buzz"
         console.log('Buzz');
-    } else if (i % 3 == 0 && i % 5 == 0) {
-    //al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
-        console.log('FizzBuzz');
+    } else if (i % 3 == 0) {
+    //al posto dei multipli sia di 3
+        console.log('Fizz');
     } else {
     console.log(i);
     }
